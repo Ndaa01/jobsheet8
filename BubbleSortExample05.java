5439b406cacb0515012a0c137b740c2e0b43f019
Ndaa01 <ananda.idn12345@gmail.com>
2023-10-23 15:37:32 +0700

    Final Stage

--- BubbleSortExample05.java
+++ BubbleSortExample05.java
@@ -1,19 +1,18 @@
 public class BubbleSortExample05 {
     public static void main(String[] args) {
-        int [] intData = {34, 18, 87, 72, 32, 54, 43};
-        int temp = 0;
+        int[] intData = {34 , 18, 87, 72, 32, 54, 43};
+        int temp=0;
 
         for (int i = 0; i < intData.length; i++) {
-            for (int j = 1; j < intData.length-i; j++) {
-                if (intData [j-1] > intData[j]) {
-                    temp = intData[j];
-                    intData [j] = intData [j-1];
-                    intData [j-1]= temp;
+            for (int j = i+1; j < intData.length; j++) {
+                if (intData[i] < intData[j]) {
+                    temp = intData[i];
+                    intData[i] = intData[j];
+                    intData[j] = temp;
                 }
-            }
+            }   
         }
-
-        System.out.println("Hasil Pengurutan: ");
+        System.out.println("Hasil pengurutan: ");
         for (int i = 0; i < intData.length; i++) {
             System.out.println(intData[i]);
         }
