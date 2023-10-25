56d2db823e4ff19bd8a14540e691a4a375b7cddd
Ndaa01 <ananda.idn12345@gmail.com>
2023-10-25 12:20:58 +0700

    Final Stage

--- LinearSearch05.java
+++ LinearSearch05.java
@@ -1,17 +1,27 @@
+import java.util.Scanner;
 public class LinearSearch05 {
     public static void main(String[] args) {
-        
-        int [] arrayInt = {34, 18, 26, 48, 72, 20, 56, 63};
-        int key = 20;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukan jumlah elemen array: ");
+        int jmlArray = sc.nextInt();
+        int [] arrayInt = new int[jmlArray];
         int hasil = 0;
 
-        for (int i = 0; i < arrayInt.length; i++) {
+        for (int i = 0; i < jmlArray; i++) {
+            System.out.print("Masukan elemen array ke- " + i + " : ");
+            arrayInt[i] = sc.nextInt();
+        }
+        System.out.print("Masukan key yang ingin dicari : ");
+        int key = sc.nextInt();
+        for (int i = 0; i < jmlArray; i++){
             if (arrayInt[i] == key) {
                 hasil = i;
+                System.out.println("Key ada dalam array pada posisi indeks ke- " +hasil);
+                break;
+            } else {
+                System.out.println("Key tidak ditemukan");
                 break;
             }
         }
-
-        System.out.println("Key ada dalam array pada posisi indeks ke- " +hasil);
     }
 }
